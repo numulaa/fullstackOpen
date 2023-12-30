84deb0a371f8001a70510110d7ae3c022200ea5c
Nurul Mukhlisa <numulaa@gmail.com>
2023-12-31 01:19:42 +0800

    added put method

--- part2_note/src/App.jsx
+++ part2_note/src/App.jsx
@@ -23,11 +23,12 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
-    console.log(noteObject);
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObject).then((res) => {
+      console.log(res);
+      setNotes(notes.concat(res.data));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -38,6 +39,17 @@ const App = () => {
     ? notes
     : notes.filter((note) => note.important === true);
 
+  const toggleImportance = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    //find the note with the selected id
+    const note = notes.find((n) => n.id === id);
+    //change the selected note with the new importance value
+    const changedNote = { ...note, important: !note.important };
+    axios.put(url, changedNote).then((res) => {
+      //create a new array with the changedNote data
+      setNotes(notes.map((n) => (n.id !== id ? n : res.data)));
+    });
+  };
   return (
     <div>
       <h1>Notes</h1>
@@ -49,7 +61,11 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportance(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
