99051443fa7ef4ca6d59a5eca90d8b88a43569b3
Nurul Mukhlisa <numulaa@gmail.com>
2024-01-03 23:36:35 +0800

    fullstack app has been built

--- phonebook_be/index.js
+++ phonebook_be/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.static("dist"));
 
 let persons = [
   {
@@ -82,7 +85,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
